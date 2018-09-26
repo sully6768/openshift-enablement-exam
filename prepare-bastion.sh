b4d53e9c02f7109e89f99f6413fcbdc4bc74a888
SES <sully6768@gmail.com>
2018-09-26 14:29:04 -0500

    updating to 3.10

--- prepare-bastion.sh
+++ prepare-bastion.sh
@@ -53,9 +53,6 @@ ssh -t `gcloud compute addresses list | grep ose-bastion | awk '{print $3}'` -o 
 ssh -t `gcloud compute addresses list | grep ose-bastion | awk '{print $3}'` -o SendEnv=RHN_USERNAME -o SendEnv=RHN_PASSWORD -o SendEnv=DNS_DOMAIN -o SendEnv=RHN_SUB_POOL -o SendEnv=BASTION_USERNAME 'sed -i "s/node[1:3]/node[1:$OCP_NODE_COUNT]/g" /home/$BASTION_USERNAME/openshift-enablement-exam/hosts'
 
 
-
-
-
 #delete temporary key files
 rm id_rsa.pub my_id.pub
 
